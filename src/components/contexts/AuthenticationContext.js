fe71753ce638b042b5325a27d38123b9baa078df
Avival <chrisFJ.avival@gmail.com>
2025-06-27 22:24:12 -0600

    Added UserContext to AuthenticationContext.

--- src/components/contexts/AuthenticationContext.js
+++ src/components/contexts/AuthenticationContext.js
@@ -1,19 +1,55 @@
 "use client"
 
-import { createContext, useContext } from "react"
+// react imports
+import { createContext, useContext, useState, useEffect } from "react"
+
+// contexts
+import { UserContext } from "/components/contexts/UserContext"
 
 const defaultAuthentication = {
     loading: false,
     login: () => Promise.resolve(),
+    logout: () => null,
     user: null
 }
 
 const AuthenticationContext = createContext( defaultAuthentication )
 
 export const AuthenticationProvider = ( { children } ) => {
+    const [ loading, setLoading ] = useState( false )
+    const [ loggedIn, setLoggedIn ] = useState( false )
+    const [ user, setUser ] = useState( defaultAuthentication.user )
+
+    const { 
+        passwordExpired, 
+        setPasswordExpired, 
+        id, 
+        setId, 
+        role, 
+        setRole 
+    } = useContext( UserContext )
+
+    useEffect( () => {
+        if( id && role ) {
+            setUser( {
+                id: id,
+                passwordExpired: "N",
+                role: role
+            } )
+        }
+    }, [] )
+
+    const login = async ( { setErrors, setError, ...params } ) => {
+
+    }
+
+    const logout = () => {}
 
     const authenticationValues = {
-
+        loading,
+        login,
+        logout,
+        user
     }
 
     return (
