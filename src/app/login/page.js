123a766851a580211a2b4f08fdd30b1ae6df6f9c
Avival <chrisFJ.avival@gmail.com>
2025-07-15 19:20:10 -0600

    Added implemented components to login page. Added configuration file for i18n -> used in localization.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,19 @@
+import loginStyles from "./login.module.css"
+
+// Custom Components
+import LoginForm from "@/components/login/LoginForm"
+
 export default function Login() {
     return (
-        <div>
-            <h1>Login Page in Development!</h1>
+        <div className={ loginStyles[ "main-column-placeholder" ] }>
+            <div className={ loginStyles[ "top-horizontal-placeholder" ] }></div>
+            <div className={ loginStyles[ "middle-horizontal-placeholder" ] }>
+                <div className={ loginStyles[ "empty-column" ] }></div>
+                <div className={ loginStyles[ "login-form-column" ] }>
+                    <LoginForm />
+                </div>
+            </div>
+            <div className={ loginStyles[ "bottom-horizontal-placeholder" ] }></div>
         </div>
     )
 }
